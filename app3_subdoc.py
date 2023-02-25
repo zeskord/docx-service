5fb9803c89d424c1fbc5efca94d24f4abf145c79
zeskord <zeskord@gmail.com>
2023-02-25 19:04:30 +0300

    Работает размещение картинок плиткой

--- app3_subdoc.py
+++ app3_subdoc.py
@@ -1,24 +1,32 @@
 # Возвращает поддокумент приложения В.
-import io, base64
+import io
+import base64
+from docx.shared import Inches, Mm
+
 
 def app3_subdoc(data, doc):
     sd = doc.new_subdoc()
     table = sd.add_table(rows=0, cols=2)
+    counter = 0
     for file_entry in data["КартинкиПриложенияВ"]:
-        row = table.add_row()
+        counter += 1
+        if counter % 2 != 0:
+            row = table.add_row()
+        else:
+            row = table.rows[len(table.rows) - 1]
         file_guid = file_entry["ИмяФайла"]
         file_extension = file_entry["Расширение"]
-        row.cells[0].text = file_guid
         file_base64 = file_entry["ДанныеФайла"]
-        bytesio_object = io.BytesIO(base64.decodebytes(bytes(file_base64, "utf-8")))
-        with open(f'workdir/{file_guid}.{file_extension}', "w+") as f:
-            (bytesio_object.getbuffer())
-            
-        
+        image = base64.b64decode(file_base64)
+        filename = f'workdir/{file_guid}.{file_extension}'
+        with open(filename, "wb") as file:
+            file.write(image)
 
-        row.cells[1].text = file_entry["ДанныеФайла"]
-        # sd.add_paragraph(file_entry["ИмяФайла"])
+        # Добавляем картинку в файл.
+        current_col = 0 if counter % 2 == 0 else 1
 
+        row.cells[current_col].paragraphs[0].add_run().add_picture(
+            filename, width = Mm(70), height = None)
 
     sd.add_paragraph("Тест!!!")
     return sd
